3f58f14e70f18de254a2984c8934766b3a5c3856
Rafael Enes <rafaeleneswork@gmail.com>
2022-12-19 08:07:00 -0300

    adicionando config server

---
+++ config-server/src/test/java/br/com/erudio/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.erudio.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
